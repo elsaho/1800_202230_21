d2107a0faf961fa086292a17a7b02ba364f1ab05
Mai Vu <vhmai3007@gmail.com>
2022-11-13 18:04:21 -0800

    split rides collection and filter displayed ridesby user role

--- scripts/main.js
+++ scripts/main.js
@@ -24,37 +24,50 @@ function addTripInfo() {
 
     // ride = db.collection("rides");
     console.log("in");
-  
+
     var startLocation = document.getElementById("startLocation").value; //get the value of the field with id="nameInput"
     var endLocation = document.getElementById("endLocation").value; //get the value of the field with id="schoolInput"
     var depTime = document.getElementById("depTime").value; //get the value of the field with id="cityInput"
     var curStatus = document.querySelector('input[name="status"]:checked').value;
-  
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        var currentUser = db.collection("users").doc(user.uid)
-        var userID = user.uid;
-        //get the document for current user.
-        currentUser.get()
-          .then(userDoc => {
-            db.collection("rides").add({
-              start: startLocation,
-              end: endLocation,
-              DepartureTime: depTime,
-              Status: curStatus,
-              userID: userID,
-              role: role,
-              timestamp: firebase.firestore.FieldValue.serverTimestamp()
-            }).then(() => {
-              window.location.href = "trip.html"; //new line added
-            })
-          })
-  
-      } else {
-        // No user is signed in.
-      }
-    });
-  
-  }
 
-  
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            var currentUser = db.collection("users").doc(user.uid)
+            var userID = user.uid;
+            //get the document for current user.
+            currentUser.get()
+                .then(userDoc => {
+                    if (role === "Passenger") {
+                        db.collection("rides").doc("AllPassengerRides").collection("PassengerRides").add({
+                            start: startLocation,
+                            end: endLocation,
+                            DepartureTime: depTime,
+                            Status: curStatus,
+                            userID: userID,
+                            role: role,
+                            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+                        }).then(() => {
+                            window.location.href = "trip.html"; //new line added
+                        })
+                    } else if (role === "Driver") {
+                        db.collection("rides").doc("AllDriverRides").collection("DrivererRides").add({
+                            start: startLocation,
+                            end: endLocation,
+                            DepartureTime: depTime,
+                            Status: curStatus,
+                            userID: userID,
+                            role: role,
+                            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+                        }).then(() => {
+                            window.location.href = "trip.html"; //new line added
+                        })
+                    }
+
+                })
+
+        } else {
+            // No user is signed in.
+        }
+    });
+
+}
--- scripts/ride.js
+++ scripts/ride.js
@@ -3,27 +3,42 @@ var role = localStorage.getItem('role');
 function populateRidesInfo() {
   let rideCardTemp = document.getElementById("rideCardTemp");
   let rideCardGroup = document.getElementById("rideCardGroup");
-
-  db.collection("rides").get()
-    .then(allRides => {
-      allRides.forEach(doc => {
-        var startLocation = doc.data().start; 
-        var endLocation = doc.data().end; 
-        var depTime = doc.data().DepartureTime;
-        var curStatus = doc.data().Status;
-        let rideCard = rideCardTemp.content.cloneNode(true);
-        rideCard.querySelector('.start').innerHTML = "From: " + startLocation;
-        rideCard.querySelector('.end').innerHTML = "To: " +endLocation;
-        rideCard.querySelector('.depTime').innerHTML = "Departing at: " + depTime;
-        rideCard.querySelector('.curStatus').innerHTML = "Status: " + curStatus;
-        if (role === 'Passenger') {
-          rideCard.querySelector('.curStatus').innerHTML = "I'm a passenger, I'm looking for a ride."
-        } else if (role === 'Driver') {
+  if (role === "Passenger") {
+    db.collection("rides").doc("AllDriverRides").collection("DrivererRides").get()
+      .then(allRides => {
+        allRides.forEach(doc => {
+          var startLocation = doc.data().start;
+          var endLocation = doc.data().end;
+          var depTime = doc.data().DepartureTime;
+          var curStatus = doc.data().Status;
+          let rideCard = rideCardTemp.content.cloneNode(true);
+          rideCard.querySelector('.start').innerHTML = "From: " + startLocation;
+          rideCard.querySelector('.end').innerHTML = "To: " + endLocation;
+          rideCard.querySelector('.depTime').innerHTML = "Departing at: " + depTime;
+          rideCard.querySelector('.curStatus').innerHTML = "Status: " + curStatus;
           rideCard.querySelector('.curStatus').innerHTML = "I'm a driver, I'm offering a ride."
-        }
-        rideCardGroup.appendChild(rideCard);
+          rideCardGroup.appendChild(rideCard);
+        })
       })
-    })
+  } else if (role === "Driver") {
+    db.collection("rides").doc("AllPassengerRides").collection("PassengerRides").get()
+      .then(allRides => {
+        allRides.forEach(doc => {
+          var startLocation = doc.data().start;
+          var endLocation = doc.data().end;
+          var depTime = doc.data().DepartureTime;
+          var curStatus = doc.data().Status;
+          let rideCard = rideCardTemp.content.cloneNode(true);
+          rideCard.querySelector('.start').innerHTML = "From: " + startLocation;
+          rideCard.querySelector('.end').innerHTML = "To: " + endLocation;
+          rideCard.querySelector('.depTime').innerHTML = "Departing at: " + depTime;
+          rideCard.querySelector('.curStatus').innerHTML = "Status: " + curStatus;
+          rideCard.querySelector('.curStatus').innerHTML = "I'm a passenger, I'm looking for a ride."        
+          rideCardGroup.appendChild(rideCard);
+        })
+      })
+
+  }
+
 }
 populateRidesInfo();
-
